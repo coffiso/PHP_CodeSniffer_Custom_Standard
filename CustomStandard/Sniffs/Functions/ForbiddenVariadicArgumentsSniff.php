eb4f579c9f16c8db7dede4a9137456e121ae95bd
Genki Shibata <59763007+GMQS@users.noreply.github.com>
2025-02-04 09:50:26 +0900

    オーバーライドしたメソッドを考慮していなかったので例外設定を追加

--- CustomStandard/Sniffs/Functions/ForbiddenVariadicArgumentsSniff.php
+++ CustomStandard/Sniffs/Functions/ForbiddenVariadicArgumentsSniff.php
@@ -4,6 +4,7 @@ namespace CustomStandard\Sniffs\Functions;
 
 use PHP_CodeSniffer\Files\File;
 use PHP_CodeSniffer\Sniffs\Sniff;
+use SlevomatCodingStandard\Helpers\DocCommentHelper;
 
 class ForbiddenVariadicArgumentsSniff implements Sniff {
     public function register() {
@@ -15,6 +16,14 @@ class ForbiddenVariadicArgumentsSniff implements Sniff {
     }
 
     public function process(File $phpcsFile, $stackPtr) {
+        $tokens = $phpcsFile->getTokens();
+        $code = $tokens[$stackPtr]["code"];
+
+        if ($code === T_FUNCTION
+            && DocCommentHelper::hasInheritdocAnnotation($phpcsFile, $stackPtr) === true) {
+            return;
+        }
+
         $functionParameters = $phpcsFile->getMethodParameters($stackPtr);
         foreach ($functionParameters as $parameter) {
             if (is_int($parameter["variadic_token"]) === true) {
