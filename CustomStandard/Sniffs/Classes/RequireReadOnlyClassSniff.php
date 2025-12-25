0efb1fc54299c5b9376e22f7cacb3100f48f26f3
Genki Shibata <59763007+GMQS@users.noreply.github.com>
2025-12-25 10:57:24 +0900

    エラーコメントを変更

--- CustomStandard/Sniffs/Classes/RequireReadOnlyClassSniff.php
+++ CustomStandard/Sniffs/Classes/RequireReadOnlyClassSniff.php
@@ -19,48 +19,48 @@ class RequireReadOnlyClassSniff implements Sniff {
 
     public function process(File $phpcsFile, $stackPtr) {
         $tokens = $phpcsFile->getTokens();
-        
+
         // T_CLASSトークンの場合はクラス全体をチェック
         if ($tokens[$stackPtr]["code"] === T_CLASS) {
             $this->processClass($phpcsFile, $stackPtr);
             return;
         }
-        
+
         // プロパティの場合の処理
         $this->processProperty($phpcsFile, $stackPtr);
     }
-    
+
     /**
      * クラス全体をチェックして、readonly classへの昇華が必要か判定する
      */
     private function processClass(File $phpcsFile, int $classPtr): void {
         $tokens = $phpcsFile->getTokens();
-        
+
         // 無名クラスはスキップ
         if ($tokens[$classPtr]["code"] === T_ANON_CLASS) {
             return;
         }
-        
+
         // 継承しているクラスはスキップ
         if ($this->isExtendingClass($phpcsFile, $classPtr)) {
             return;
         }
-        
+
         // readonly classかどうかをチェック
         $isReadonlyClass = $this->isReadonlyClass($phpcsFile, $classPtr);
-        
+
         // クラスのプロパティを取得
         $properties = $this->getClassProperties($phpcsFile, $classPtr);
-        
+
         if (count($properties) === 0) {
             // プロパティがない場合はチェックしない（readonly classにする意味がない）
             return;
         }
-        
+
         // 各プロパティのreadonly状態をチェック
         $readonlyCount = 0;
         $nonStaticCount = 0;
-        
+
         foreach ($properties as $property) {
             if ($property['is_static']) {
                 continue;
@@ -70,22 +70,22 @@ class RequireReadOnlyClassSniff implements Sniff {
                 $readonlyCount++;
             }
         }
-        
+
         // プロパティが全て static の場合はスキップ
         if ($nonStaticCount === 0) {
             return;
         }
-        
+
         // 全てのプロパティがreadonlyの場合、readonly classに昇華すべき
         if ($readonlyCount === $nonStaticCount && $readonlyCount > 0) {
             if (!$isReadonlyClass) {
                 // 継承可能なクラス（abstract または non-final）の場合は自動修正を無効化
                 $isInheritable = $this->isInheritableClass($phpcsFile, $classPtr);
-                
+
                 if ($isInheritable) {
                     // 継承可能なクラスの場合はエラーのみ（自動修正なし）
                     $phpcsFile->addError(
-                        'All properties are readonly. Class should be declared as readonly and readonly modifiers should be removed from properties. Auto-fix is disabled for inheritable classes (abstract or non-final)',
+                        'All properties are readonly. Class should be declared as readonly and readonly modifiers should be removed from properties',
                         $classPtr,
                         'ShouldBeReadOnlyClass'
                     );
@@ -96,35 +96,35 @@ class RequireReadOnlyClassSniff implements Sniff {
                         $classPtr,
                         'ShouldBeReadOnlyClass'
                     );
-                    
+
                     if ($fix === true) {
                         // classキーワードより前にあるfinal, abstractなどの修飾子を探し、
                         // readonlyをそれらの前（最も早い位置）に追加する
                         $phpcsFile->fixer->beginChangeset();
-                        
+
                         // classキーワードの前を遡って、final/abstractを探す
                         $insertBeforePtr = $classPtr;
                         $checkPtr = $classPtr - 1;
                         while ($checkPtr > 0) {
-                            if ($tokens[$checkPtr]['code'] === T_WHITESPACE || 
-                                $tokens[$checkPtr]['code'] === T_COMMENT || 
+                            if ($tokens[$checkPtr]['code'] === T_WHITESPACE ||
+                                $tokens[$checkPtr]['code'] === T_COMMENT ||
                                 $tokens[$checkPtr]['code'] === T_DOC_COMMENT) {
                                 $checkPtr--;
                                 continue;
                             }
-                            
-                            if ($tokens[$checkPtr]['code'] === T_FINAL || 
+
+                            if ($tokens[$checkPtr]['code'] === T_FINAL ||
                                 $tokens[$checkPtr]['code'] === T_ABSTRACT) {
                                 $insertBeforePtr = $checkPtr;
                                 $checkPtr--;
                                 continue;
                             }
-                            
+
                             break;
                         }
-                        
+
                         $phpcsFile->fixer->addContentBefore($insertBeforePtr, 'readonly ');
-                        
+
                         // 全てのプロパティからreadonlyを削除
                         foreach ($properties as $property) {
                             if ($property['has_readonly'] && $property['readonly_ptr'] !== null) {
@@ -156,7 +156,7 @@ class RequireReadOnlyClassSniff implements Sniff {
             );
         }
     }
-    
+
     /**
      * プロパティの個別チェック
      */
@@ -182,18 +182,18 @@ class RequireReadOnlyClassSniff implements Sniff {
         if (DocCommentHelper::hasInheritdocAnnotation($phpcsFile, $kindPtr) === true) {
             return;
         }
-        
+
         // プロパティが属するクラスを取得
         $classPtr = $this->findClassPointer($phpcsFile, $stackPtr);
         if ($classPtr === null) {
             return;
         }
-        
+
         // 継承しているクラスのプロパティはスキップ
         if ($this->isExtendingClass($phpcsFile, $classPtr)) {
             return;
         }
-        
+
         // readonly classの場合、プロパティにreadonlyは不要（あれば削除を提案）
         if ($this->isReadonlyClass($phpcsFile, $classPtr)) {
             $readonlyPtr = $this->findReadonlyModifier($phpcsFile, $stackPtr, $kindPtr);
@@ -203,7 +203,7 @@ class RequireReadOnlyClassSniff implements Sniff {
                     $readonlyPtr,
                     'RedundantReadOnlyModifier'
                 );
-                
+
                 if ($fix === true) {
                     $phpcsFile->fixer->beginChangeset();
                     $phpcsFile->fixer->replaceToken($readonlyPtr, '');
@@ -228,7 +228,7 @@ class RequireReadOnlyClassSniff implements Sniff {
             }
             $ptr++;
         }
-        
+
         $ptr = $stackPtr - 1;
         while (isset($tokens[$ptr]["code"]) === true && $tokens[$ptr]["code"] === T_WHITESPACE) {
             $ptr--;
@@ -249,13 +249,13 @@ class RequireReadOnlyClassSniff implements Sniff {
             [$varName]
         );
     }
-    
+
     /**
      * クラスがreadonly classかどうかを判定
      */
     private function isReadonlyClass(File $phpcsFile, int $classPtr): bool {
         $tokens = $phpcsFile->getTokens();
-        
+
         // classキーワードの前にreadonlyがあるかチェック
         $ptr = $classPtr - 1;
         while ($ptr > 0) {
@@ -263,106 +263,106 @@ class RequireReadOnlyClassSniff implements Sniff {
                 $ptr--;
                 continue;
             }
-            
+
             if ($tokens[$ptr]['code'] === T_READONLY) {
                 return true;
             }
-            
+
             // abstract, final などの前にreadonlyがあるかもしれない
             if ($tokens[$ptr]['code'] === T_ABSTRACT || $tokens[$ptr]['code'] === T_FINAL) {
                 $ptr--;
                 continue;
             }
-            
+
             break;
         }
-        
+
         return false;
     }
-    
+
     /**
      * クラスが他のクラスを継承しているかチェック
      */
     private function isExtendingClass(File $phpcsFile, int $classPtr): bool {
         $tokens = $phpcsFile->getTokens();
-        
+
         if (!isset($tokens[$classPtr]['scope_opener'])) {
             return false;
         }
-        
+
         $scopeOpener = $tokens[$classPtr]['scope_opener'];
-        
+
         // classキーワードからスコープ開始までの間にextendsがあるかチェック
         $extendsPtr = $phpcsFile->findNext(T_EXTENDS, $classPtr + 1, $scopeOpener);
-        
+
         return $extendsPtr !== false;
     }
-    
+
     /**
      * クラスが継承可能かどうかをチェック（abstract または non-final）
      */
     private function isInheritableClass(File $phpcsFile, int $classPtr): bool {
         $tokens = $phpcsFile->getTokens();
-        
+
         // classキーワードの前にabstractがあるかチェック
         $ptr = $classPtr - 1;
         while ($ptr > 0) {
-            if ($tokens[$ptr]['code'] === T_WHITESPACE || 
-                $tokens[$ptr]['code'] === T_COMMENT || 
+            if ($tokens[$ptr]['code'] === T_WHITESPACE ||
+                $tokens[$ptr]['code'] === T_COMMENT ||
                 $tokens[$ptr]['code'] === T_DOC_COMMENT) {
                 $ptr--;
                 continue;
             }
-            
+
             if ($tokens[$ptr]['code'] === T_ABSTRACT) {
                 return true;
             }
-            
+
             if ($tokens[$ptr]['code'] === T_FINAL) {
                 return false;
             }
-            
+
             // readonly の場合は続けて探索
             if ($tokens[$ptr]['code'] === T_READONLY) {
                 $ptr--;
                 continue;
             }
-            
+
             break;
         }
-        
+
         // final が見つからなかった場合は継承可能（non-final）
         return true;
     }
-    
+
     /**
      * クラス内の全プロパティを取得
      */
     private function getClassProperties(File $phpcsFile, int $classPtr): array {
         $tokens = $phpcsFile->getTokens();
-        
+
         if (!isset($tokens[$classPtr]['scope_opener']) || !isset($tokens[$classPtr]['scope_closer'])) {
             return [];
         }
-        
+
         $scopeOpener = $tokens[$classPtr]['scope_opener'];
         $scopeCloser = $tokens[$classPtr]['scope_closer'];
-        
+
         $properties = [];
         $ptr = $scopeOpener + 1;
-        
+
         while ($ptr < $scopeCloser) {
             if ($tokens[$ptr]['code'] === T_VARIABLE) {
                 // プロパティかどうか確認
                 if (PropertyHelper::isProperty(phpcsFile: $phpcsFile, variablePointer: $ptr, promoted: true)) {
                     // アクセス修飾子を見つける
                     $modifierPtr = $phpcsFile->findPrevious([T_PUBLIC, T_PROTECTED, T_PRIVATE], $ptr - 1);
-                    
+
                     if ($modifierPtr !== false) {
                         $isStatic = false;
                         $hasReadonly = false;
                         $readonlyPtr = null;
-                        
+
                         // readonly と static をチェック
                         $checkPtr = $modifierPtr;
                         while ($checkPtr < $ptr) {
@@ -375,7 +375,7 @@ class RequireReadOnlyClassSniff implements Sniff {
                             }
                             $checkPtr++;
                         }
-                        
+
                         // アクセス修飾子の前もチェック
                         $beforePtr = $modifierPtr - 1;
                         while ($beforePtr > $scopeOpener) {
@@ -392,7 +392,7 @@ class RequireReadOnlyClassSniff implements Sniff {
                             }
                             break;
                         }
-                        
+
                         $properties[] = [
                             'ptr' => $ptr,
                             'modifier_ptr' => $modifierPtr,
@@ -405,16 +405,16 @@ class RequireReadOnlyClassSniff implements Sniff {
             }
             $ptr++;
         }
-        
+
         return $properties;
     }
-    
+
     /**
      * プロパティのreadonlyモディファイアを検索
      */
     private function findReadonlyModifier(File $phpcsFile, int $modifierPtr, int $variablePtr): ?int {
         $tokens = $phpcsFile->getTokens();
-        
+
         // アクセス修飾子と変数の間をチェック
         $ptr = $modifierPtr + 1;
         while ($ptr < $variablePtr) {
@@ -423,7 +423,7 @@ class RequireReadOnlyClassSniff implements Sniff {
             }
             $ptr++;
         }
-        
+
         // アクセス修飾子の前をチェック
         $ptr = $modifierPtr - 1;
         while ($ptr > 0) {
@@ -436,16 +436,16 @@ class RequireReadOnlyClassSniff implements Sniff {
             }
             break;
         }
-        
+
         return null;
     }
-    
+
     /**
      * プロパティが属するクラスのポインタを取得
      */
     private function findClassPointer(File $phpcsFile, int $ptr): ?int {
         $tokens = $phpcsFile->getTokens();
-        
+
         // 現在の位置から親のスコープを辿ってクラスを探す
         while ($ptr > 0) {
             if (isset($tokens[$ptr]['conditions'])) {
@@ -457,7 +457,7 @@ class RequireReadOnlyClassSniff implements Sniff {
             }
             $ptr--;
         }
-        
+
         return null;
     }
 }
